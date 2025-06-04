518e5f513c3d8a1ba85268a7c4289c9eb5fee1a8
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-04 14:35:15 +0200

    snack 02 completion

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,31 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function initials(name_list){
+    let first_letters = []
+
+    for(i = 0; i < name_list.length; i++){
+        element = name_list[i]
+        first_letters.push(element.charAt(0))
+    }
+    return first_letters
+}
 
 
-// Invoca la funzione qui e stampa il risultato in console
+// Invoca la funzione qui e stampa il risultato in console 
+console.log(initials(names))
 
 
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"] 
 
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+//Bonus :3
+const initial_name = name_list => {
+    let first_letters = []
+    for(i = 0; i < name_list.length; i++){
+        element = name_list[i]
+        first_letters.push(element.charAt(0))
+    }
+    return first_letters
+}
+
+console.log(initial_name(names))
